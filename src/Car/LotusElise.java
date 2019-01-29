1f30bc0df70ae29ea02b88f91ad8c5e1b8597699
git config --global user.name Elise.mellbin <git config --global user.email elisemellbin@gmail.com>
2019-01-29 16:32:21 +0100

    Lotus Elise

---
+++ src/Car/LotusElise.java
@@ -0,0 +1,18 @@
+package Car;
+
+import javafx.scene.paint.Color;
+
+public class LotusElise extends Car {
+
+
+		public LotusElise() { //bilens attributer bestämms
+			super(125, "LotusElise", Color.BLACK); //enginepower, namn, färg
+			stopEngine();//som grund inställning är motorn avstängd
+		}
+		public double speedFactor() {//Speedfactor räknar ut accelerationen. om turbo är på ökar hastigheten
+			double FF = 1;
+			return getEnginePower() * 0.01 * FF;
+		}
+
+	}
+
