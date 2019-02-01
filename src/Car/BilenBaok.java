4f414b07855b4d082b0c12dc74f0e0076e2ecbf6
git config --global user.name Elise.mellbin <git config --global user.email elisemellbin@gmail.com>
2019-02-01 12:39:35 +0100

    BilenBaok

---
+++ src/Car/BilenBaok.java
@@ -0,0 +1,20 @@
+package Car;
+
+import javafx.scene.paint.Color;
+
+public class BilenBaok extends Car {
+
+
+			public BilenBaok() { //bilens attributer bestämms
+				super(75, "BilenBaok", Color.SADDLEBROWN); //enginepower, namn, färg
+				stopEngine();//som grund inställning är motorn avstängd
+			}
+			public double speedFactor() {//Speedfactor räknar ut accelerationen. om turbo är på ökar hastigheten
+				double FF = 1;
+				return getEnginePower() * 0.01 * FF;
+			}
+
+		}
+	
+	
+}
