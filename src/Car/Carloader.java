f43425271d37ea5bec0f68988f3d95bfccbbae18
git config --global user.name Elise.mellbin <git config --global user.email elisemellbin@gmail.com>
2019-03-01 12:08:19 +0100

    Update Carloader.java

--- src/Car/Carloader.java
+++ src/Car/Carloader.java
@@ -3,10 +3,10 @@ package Car;
 public class Carloader { //arraylist med cars för de bilar som kan ha bilar på sig, skapa en restriction för varje bil.
 
 	private Car Loaded;
-	public void loadCar(Car c) {
+	public void loadCar(Car c) { //Gör så att bilen kan lasta på en till bil 
 		Loaded = c;
 	}
-	public Car unloadCar() {
+	public Car unloadCar() { //Gör så att bilen kan avlasta en bil 
 		return Loaded;
 	}
 }
